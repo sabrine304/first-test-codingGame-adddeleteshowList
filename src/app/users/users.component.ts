a02a931f54b3a1c1b1692b0d469aa95c0edd2dab
sabrine304 <ourabisabrine3@gmail.com>
2025-01-09 16:22:37 +0100

    Add pipe file and use it in component

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { UserService } from '../user.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { UppercasePipe } from '../uppercase.pipe';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,UppercasePipe],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
